9c9283e193a3b4001185e4694a5a90460940dfa1
modernsilpi <modernsilpi@gmail.com>
2021-12-27 16:19:18 +0530

    login api bugs fixed

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:mvc_pattern/mvc_pattern.dart';
